7c471cb2042b55eb57210c7394fdefd0e10bd7d5
Jamie95187 <jamiewong63@googlemail.com>
2020-03-12 17:25:08 +0000

    feat: display component

--- src/components/Calculator/Calulator.spec.js
+++ src/components/Calculator/Calulator.spec.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Calculator from './Calculator';
+import Display from '../Display/Display';
 
 describe('Calculator', () => {
   let wrapper;
 
   beforeEach(() => wrapper = shallow(<Calculator />));
 
-  it('it should render a <div />', () => {
+  it('should render a <div />', () => {
     expect(wrapper.find('div').length).toEqual(1);
   });
+
+  it('should render the Display Component', () => {
+    expect(wrapper.containsMatchingElement(<Display displayValue={wrapper.instance().state.displayValue}/>)).toEqual(true);
+  });
+
 });

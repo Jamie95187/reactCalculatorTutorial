243b3eb042c6822c2236424c11eb2b1ee57d9b92
Jamie95187 <jamiewong63@googlemail.com>
2020-03-12 19:25:34 +0000

    css: for display component

--- src/components/Display/Display.spec.js
+++ src/components/Display/Display.spec.js
@@ -7,8 +7,15 @@ describe('Display', () => {
 
   beforeEach(() => wrapper = shallow(<Display displayValue={''}/>));
 
+  it('should render correctly', () => expect(wrapper).toMatchSnapshot());
+
   it('should render a <div />', () => {
     expect(wrapper.find('div').length).toEqual(1);
   });
+
+  it('renders the value of displayValue', () => {
+    wrapper.setProps({ displayValue: 'test' });
+    expect(wrapper.text()).toEqual('test');
+  })
 
 });

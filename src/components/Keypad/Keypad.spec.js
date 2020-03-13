fee5da748a0652feb8856ac9bbe12d780f03690a
Jamie95187 <jamiewong63@googlemail.com>
2020-03-13 23:43:55 +0000

    feat: add key values and refactor keypad tests

--- src/components/Keypad/Keypad.spec.js
+++ src/components/Keypad/Keypad.spec.js
@@ -1,33 +1,56 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import Keypad from './Keypad';
+import Key from '../Key/Key';
 
-describe('keypad', () => {
+describe('Keypad', () => {
   let wrapper;
 
   beforeEach(() => {
     wrapper = shallow(
       <Keypad
-      callOperator={jest.fn()}
-      numbers={[]}
-      operators={[]}
-      setOperator={jest.fn()}
-      updateDisplay={jest.fn()}
+        callOperator={jest.fn()}
+        numbers={[]}
+        operators={[]}
+        setOperator={jest.fn()}
+        updateDisplay={jest.fn()}
       />
     );
   });
 
-  it("should render 3 <div />'s", () => {
-    expect(wrapper.find('div').length).toEqual(3);
+  it('should render an instance of the Key component for each index of numbers, operators and the submit Key', () => {
+    const numbers = ['0', '1'];
+    const operators = ['+', '-'];
+    const submit = 1;
+    const keyTotal = numbers.length + operators.length + submit;
+    wrapper.setProps({numbers, operators});
+    expect(wrapper.find('Key').length).toEqual(keyTotal);
   });
 
-  it('renders the values of numbers', () => {
-    wrapper.setProps({numbers: ['0', '1', '2']});
-    expect(wrapper.find('.numbers-container').text()).toEqual('012');
-  })
-
-  it('renders the values of the operators', () => {
-    wrapper.setProps({operators: ['+','-','*','/']});
-    expect(wrapper.find(".operators-container").text()).toEqual('+-*/');
-  })
 })
+
+describe('mounted Keypad', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(
+      <Keypad
+        callOperator={jest.fn()}
+        numbers={[]}
+        operators={[]}
+        setOperator={jest.fn()}
+        updateDisplay={jest.fn()}
+      />
+    );
+  });
+
+  it('renders the values of numbers to the DOM', () => {
+    wrapper.setProps({ numbers: ['0', '1', '2'] });
+    expect(wrapper.find('.numbers-container').text()).toEqual('012');
+  });
+
+  it('renders the values of the operators to the DOM', () => {
+    wrapper.setProps({ operators: ['+', '-', '*', '/'] });
+    expect(wrapper.find('.operators-container').text()).toEqual('+-*/');
+  });
+});

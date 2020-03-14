ef7605120c28b7c3ffe73d42796c9fa7643947a2
Jamie95187 <jamiewong63@googlemail.com>
2020-03-14 01:03:31 +0000

    feat: add click events

--- src/components/Calculator/Calulator.spec.js
+++ src/components/Calculator/Calulator.spec.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import Calculator from './Calculator';
 import Display from '../Display/Display';
 import Keypad from '../Keypad/Keypad';
 
 describe('Calculator', () => {
-  
+
   it('should render correctly', () => expect(wrapper).toMatchSnapshot());
 
   let wrapper;
@@ -31,3 +31,35 @@ describe('Calculator', () => {
     )).toEqual(true)
   });
 });
+
+describe('mounted Calculator', () => {
+
+  let wrapper;
+
+  beforeEach(() => wrapper = mount(<Calculator />));
+
+  it('calls updateDisplay when a number key is clicked', () => {
+    const spy = jest.spyOn(wrapper.instance(), 'updateDisplay');
+    wrapper.instance().forceUpdate();
+    expect(spy).toHaveBeenCalledTimes(0);
+    wrapper.find('.number-key').first().simulate('click');
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+
+  it('calls setOperator when a operator key is clicked', () => {
+    const spy = jest.spyOn(wrapper.instance(), 'setOperator');
+    wrapper.instance().forceUpdate();
+    expect(spy).toHaveBeenCalledTimes(0);
+    wrapper.find('.operator-key').first().simulate('click');
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+
+  it('calls callOperator when the submit key is clicked', () => {
+    const spy = jest.spyOn(wrapper.instance(), 'callOperator');
+    wrapper.instance().forceUpdate();
+    expect(spy).toHaveBeenCalledTimes(0);
+    wrapper.find('.submit-key').first().simulate('click');
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+
+})

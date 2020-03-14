a18c240446c5650e89ff643707717a003b16768e
Jamie95187 <jamiewong63@googlemail.com>
2020-03-14 00:03:36 +0000

    snapshot: update calculator snapshot and keys displayed on UI

--- src/components/Calculator/Calulator.spec.js
+++ src/components/Calculator/Calulator.spec.js
@@ -5,11 +5,12 @@ import Display from '../Display/Display';
 import Keypad from '../Keypad/Keypad';
 
 describe('Calculator', () => {
+  
+  it('should render correctly', () => expect(wrapper).toMatchSnapshot());
+
   let wrapper;
 
   beforeEach(() => wrapper = shallow(<Calculator />));
-
-  it('should render correctly', () => expect(wrapper).toMatchSnapshot());
 
   it('should render a <div />', () => {
     expect(wrapper.find('div').length).toEqual(1);
--- src/components/Keypad/Keypad.jsx
+++ src/components/Keypad/Keypad.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Key from '../Key/Key';
 import './Keypad.css';
 
